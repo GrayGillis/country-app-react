4571b29d1e2d1c37c49ecf3a6cc04a7b73a061d3
GrayGillis <graygillis15@gmail.com>
2021-08-07 11:21:31 -0700

    cleaning up UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './Header';
+
+const api = '';
 
 function App() {
+  const [country, setCountry] = useState('');
+  const [capital, setCapital] = useState('');
+  const [population, setPopulation] = useState('');
+  const [currency, setCurrency] = useState('');
+  const [language, setLanguage] = useState('');
+  const [flag, setFlag] = useState('');
 
+  const getCountryInfo = (country) => {
+    // api call
+  }
 
   return (
     <>
       <div className="header">
-        
+        <Header />
+      </div>
+      <div className="main">
+        <input type="text" value="country" onChange={(e) => setCountry(e.target.value)}/>
+        <button onClick={}>Submit</button>
+      </div>
+      <div className="body">
+        <section>
+          <h2 className="country">Country: {country}</h2>
+          <h2 className="capital">Capital: {capital}</h2>
+          <h2 className="population">Population: {population}</h2>
+          <h2 className="currency">Currency: {currency}</h2>
+          <h2 className="language">Language: {language}</h2>
+
+          {/* for the flag */}
+          <img src="" alt="" />
+        </section>
       </div>
     </>
   );
