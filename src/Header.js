269f189272079ae17bf9d1b92f6a9b894403a96a
GrayGillis <graygillis15@gmail.com>
2021-08-07 11:05:46 -0700

    removed some files, added header

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Welcome to Grayson's Country Info App</h1>
+            <h3>This project was built using React</h3> <br />
+            <p>Please enter the country's name:</p>
+        </div>
+    )
+}
+
+export default Header
